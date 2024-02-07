b9ccb67c33fbb8ee4e67fc3f659370a770d5e2ca
reuterShawn <reutershawn45@gmail.com>
2024-02-06 20:42:13 -0600

    Add custom bg colors to use in tailwind styles

--- shawn-reuter-profolio/tailwind.config.ts
+++ shawn-reuter-profolio/tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      '--bgLight': '#508991',
+      '--bgLight2': '#9ebcc0',
+      '--btn': '#09C38E',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
