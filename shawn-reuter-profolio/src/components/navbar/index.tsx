78bfc4887c21890afa98f2b068af5b3ee5476bc5
reuterShawn <reutershawn45@gmail.com>
2024-02-07 00:25:24 -0600

    Add switch button

---
+++ shawn-reuter-profolio/src/components/navbar/index.tsx
@@ -0,0 +1,23 @@
+// import {useState} from "react";
+
+// export default fucntion Index() {
+
+
+// const [toggle, setToggle] =useState<boolean>(false);
+// return (
+//     <div className="flex h-screen items-center justify-center">
+//         <div
+//           onClick={() => setToggle(!toggle)}
+//           className={`flex h-6 w-12 cursor-pointer rounderd-full border border-black ${
+//             toggle ? "justify-start bg-white" : "justify-end bg-black "
+//         } p-[1px]``}
+//         >
+//         <motion.div
+//          className={`h-5 w-5 rounded-full ${
+//          toggle ? "bg-black"  : "bg-white"
+//         }`}
+//         />
+//         </div>
+//     </div>
+// );
+    
