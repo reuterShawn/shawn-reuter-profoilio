19500ba45d230b34e43a634f39fe1421a19038d9
reuterShawn <reutershawn45@gmail.com>
2024-02-06 20:41:10 -0600

    Add footer and navbar

---
+++ shawn-reuter-profolio/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+  return (
+    <div className="flex-row -bg--bgLight2 absolute bottom-0 h-50 w-full space-x-20 pl-5 p-3">
+      <div className="font-bold order-1 absolute">Reuter</div>
+      <div className="order-2 pl-10">
+        Shawn Reuter © All rights reserved.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
