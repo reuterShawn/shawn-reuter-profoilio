63d1c11abe35cb01ab34e8c6acfb71775cb11ceb
reuterShawn <reutershawn45@gmail.com>
2024-02-18 20:57:33 -0600

    Make footer sticky

--- shawn-reuter-profolio/src/components/footer/Footer.jsx
+++ shawn-reuter-profolio/src/components/footer/Footer.jsx
@@ -1,11 +1,8 @@
-
 const Footer = () => {
   return (
-    <div className="flex-row -bg--bgLight2 absolute bottom-0 h-50 w-full space-x-20 pl-5 p-3">
+    <div className="flex-row sticky -bg--bgLight2  bottom-0 h-50 w-full space-x-20 pl-5 p-3">
       <div className="font-bold order-1 absolute">Reuter</div>
-      <div className="order-2 pl-10">
-        Shawn Reuter © All rights reserved.
-      </div>
+      <div className="order-2 pl-10">Shawn Reuter © All rights reserved.</div>
     </div>
   );
 };
